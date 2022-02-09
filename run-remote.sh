747b7bfebc9d1fea618538f97c4ced27e154de09
Astrid Yu <astrid@astrid.tech>
2022-02-09 19:42:04 +0000

    Write a janky helper script for remotely running

---
+++ run-remote.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# A helper script to run this program on a remote device such as a raspberry pi.
+# Prior to running this script, you should:
+#  - copy over SSH keys
+#  - sudo pip install -r requirements.txt
+
+ssh_target=$1
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 ssh_target program_args ..."
+    exit 1
+fi
+
+shift 1
+
+dir=$(mktemp -d)
+
+script="
+mkdir -p $dir
+echo \"Copying files to $dir\"
+cd $dir
+
+tar x  # extract tar from stdin
+"
+
+git ls-files skeleton_stick requirements.txt resources | tar c --files-from=- | ssh $ssh_target "$script"
+
+ssh -t $ssh_target "cd $dir && sudo python3 -m skeleton_stick $@"
