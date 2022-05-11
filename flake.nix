bf1531501b6844c6e4c3a50853635f932e6227c1
Astrid Yu <astrid@astrid.tech>
2022-05-11 11:41:34 -0700

    Add rust to the flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,37 @@
 {
-  description = "Skeleton Stick";
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in {
+        defaultPackage = naersk-lib.buildPackage ./.;
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ]
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShell = import ./shell.nix { inherit pkgs; };
-        }
-      );
+        defaultApp = utils.lib.mkApp { drv = self.defaultPackage."${system}"; };
+
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              rustPackages.clippy
+              rustc
+              rustfmt
+
+              python3
+              python39Packages.virtualenv
+              python39Packages.pip
+
+              nixfmt
+              pre-commit
+              texlive.combined.scheme-full
+            ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+      });
 }
--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }: pkgs.mkShell {
-  buildInputs = with pkgs; [
-    python3
-    python39Packages.virtualenv
-    python39Packages.pip
-    texlive.combined.scheme-full
-  ];
-}
